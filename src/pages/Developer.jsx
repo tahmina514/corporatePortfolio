c052abb9e683bcbfe6fa3b1a4ccca7ad6acdd8bc
tahmina514 <tahminalubna910@gmail.com>
2026-09-08 21:29:55 +0600

    Update Lecturelia Logo

--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -8,7 +8,7 @@ import {
 import { useLanguage } from "../context/LanguageContext";
 import translations from "../translations/translations";
 
-import lectureliaLogo from "../assets/lecturelia-logo.png";
+import lectureliaLogo from "../assets/lecturelia-logo.jpeg";
 
 import "./Developer.css";
 
